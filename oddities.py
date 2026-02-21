13f0d6aab61d3ea7550c1a71909317f9952c452a
Louis Billaut <ragedfear4875@gmail.com>
2026-02-21 20:55:52 +0800

    implement mersenne exponent getter and checker

--- oddities.py
+++ oddities.py
@@ -1,10 +1,20 @@
 import constants as C
+from math import sqrt
 
 def is_natural(number: int) -> bool:
     return True if isinstance(number, int) and number > 0 else False
 
 def is_whole(number: int) -> bool:
     return True if isinstance(number, int) and number >= 0 else False
+
+def is_mersenne_number(number: int) -> bool:
+    if not is_natural(number): raise ValueError("Parameter must be a natural number!")
+    m = number + 1
+    return m & (m-1) == 0
+
+def get_mersenne_exponent(number: int) -> bool:
+    if not is_mersenne_number(number): raise ValueError("Parameter must be a mersenne prime!")
+    return (number-1).bit_length() - 1
 
 def is_mersenne_prime(exponent: int) -> bool:
     if not is_natural(exponent): raise ValueError("Parameter must be a natural number!")
@@ -21,7 +31,6 @@ def is_mersenne_prime(exponent: int) -> bool:
 def is_fermat_prime(number: int) -> bool:
     if not is_natural(number): raise ValueError("Parameter must be a natural number!")
     return number in C.FERMAT_NUMBERS and number < 65538 # Biggest known fermat prime is 65537
-
 
 def is_fermat_number(number: int) -> bool:
     if not is_natural(number): raise ValueError("Parameter must be a natural number!")
