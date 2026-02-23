57f681136a7600d72de1ab87d5009a52565f4481
Louis Billaut <ragedfear4875@gmail.com>
2026-02-24 00:10:16 +0800

    add new checkers

--- oddities.py
+++ oddities.py
@@ -25,23 +25,65 @@ def get_mersenne_exponent(number: int) -> int:
     if not is_mersenne_number(number): raise ValueError("Parameter must be a mersenne prime!")
     return (number+1).bit_length() - 1
 
-def get_aliquot_sum(n: int) -> int:
+def get_sum_of_all_divisor(n: int, aliquot: bool = False) -> int:
     if not is_natural(n): raise ValueError("Parameter must be a natural number!")
     total = 0
     i = 1
 
-    while i * i < n:
+    while i * i <= n:
         if n % i == 0:
             total += i
             k = n // i
-            if i != k: total += i
+            if i != k: total += k
         i += 1
-    return total
-
+    return total - n if aliquot else total
 
 # Checkers
 def is_perfect_number(number: int) -> bool:
-    return get_aliquot_sum(number) == number
+    return get_sum_of_all_divisor(number, aliquot = True) == number
+
+def is_quasiperfect_number(number: int) -> bool:
+    if not is_natural(number): raise ValueError("Parameter must be a natural number!")
+    return False
+
+def is_almost_perfect_number(number: int) -> bool:
+    return get_sum_of_all_divisor(number, aliquot = True) == number - 1
+
+def is_superperfect_number(number: int) -> bool:
+    return get_sum_of_all_divisor(get_sum_of_all_divisor(number)) == 2*number
+
+def is_erdos_number(number: int) -> bool:
+    if not is_natural(number): raise ValueError("Parameter must be a natural number!")
+    divisors = get_divisors(number) 
+    total = 0
+
+    for i, d in enumerate(divisors):
+        total += d
+        if total == number and i < len(divisors) - 1:
+            return True
+        elif total > number:
+            return False
+    return False
+
+def is_abundant_number(number: int) -> bool:
+    return get_sum_of_all_divisor(number, aliquot = True) > number
+
+def is_highly_abundant_number(number: int) -> bool:
+    sig_n = get_sum_of_all_divisor(number, aliquot = False)
+
+    for i in range(1, number):
+        if get_sum_of_all_divisor(i, aliquot = False) >= sig_n:
+            return False
+    return True
+
+def is_super_abundant_number(number: int) -> bool:
+    sig_n = get_sum_of_all_divisor(number, aliquot = False)
+
+    for i in range(1, number):
+        if get_sum_of_all_divisor(i, aliquot = False)/i >= sig_n/number:
+            return False
+    return True
+        
 
 def is_perfect_square(number: int) -> bool:
     if not is_natural(number): raise ValueError("Parameter must be a natural number!")
@@ -86,5 +128,6 @@ def is_sublime_number(number: int) -> bool:
     if not is_natural(number): raise ValueError("Parameter must be a natural number!")
     return number in C.SUBLIME_NUMBERS
 
-def is_quasiperfect_number(number: int) -> bool:
-    return get_aliquot_sum(number) == 2*number + 1
+def is_descartes_number(number: int) -> bool:
+    if not is_natural(number): raise ValueError("Parameter must be a natural number!")
+    return number in C.DESCARTES_NUMBERS
